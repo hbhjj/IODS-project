d37a6b95021ac88c7d67cb2f7882bc1a13774cba
hbhjj <haapojj1@t31300-la035.org.aalto.fi>
2017-02-23 13:38:37 +0200

    fixes

--- data/create_human.R
+++ data/create_human.R
@@ -21,10 +21,10 @@ gend <- rename(gii, GIIRank = GII.Rank, GII= Gender.Inequality.Index..GII.,
                  Mat.Mor = Maternal.Mortality.Ratio, 
                Ado.Birth = Adolescent.Birth.Rate,
                Parli.F = Percent.Representation.in.Parliament,
-               Edu2.FM = Population.with.Secondary.Education..Female.,
-                 EduM = Population.with.Secondary.Education..Male.,
-               Labo.FM = Labour.Force.Participation.Rate..Female.,
-                 LabM = Labour.Force.Participation.Rate..Male.)
+               Edu2.F = Population.with.Secondary.Education..Female.,
+                 Edu2.M = Population.with.Secondary.Education..Male.,
+               Labo.F = Labour.Force.Participation.Rate..Female.,
+                 Labo.M = Labour.Force.Participation.Rate..Male.)
 
 dim(gend)
 str(gend)
@@ -32,7 +32,7 @@ summary(gend)
 
 #Create the new variables
 
-gend <- mutate(gend, LabRatio = Labo.FM / LabM, EduRatio = Edu2.FM / EduM)
+gend <- mutate(gend, Labo.FM = Labo.F / Labo.M, Edu2.FM = Edu2.F / Edu2.M)
 
 #Combine the data and check if everything is okay
 human <- inner_join(humdev,gend,by= "Country")
@@ -40,13 +40,30 @@ dim(human)
 str(human)
 
 #write the data to data folder
-write.table(alc, file='data/human.txt') 
+write.table(human, file='data/human.txt') 
 
-#replace "," with "." in GNI and transform it to numeric
-human$GNI <- gsub(",", ".", human$GNI)
+#remove commas in GNI and transform it to numeric
+human$GNI <- gsub(",", "", human$GNI)
 human$GNI <- as.numeric(human$GNI)
 
 #select only certain variables
 selected_variables <- c( "Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
 human <- select(human, one_of(selected_variables))
 str(human)
+
+#remove rows with missing values
+human <- x <- human[complete.cases(human), ]
+dim(human)
+#lets see, what regions we should remove
+human$Country
+#remove regions. They are the last 7 rows
+human <- human[1:155,]
+human$Country
+
+#row.names from countries and remove Country variable
+row.names(human) <- human$Country
+human <- human[,-1]
+str(human)
+
+#write the data
+write.table(human, file='data/human.txt', row.names = TRUE) 
