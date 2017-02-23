21e3146e73add185859c595c9bfb7f21578c8ad2
hbhjj <haapojj1@t31300-la035.org.aalto.fi>
2017-02-23 12:51:58 +0200

    minor fixes

--- data/create_human.R
+++ data/create_human.R
@@ -10,7 +10,7 @@ gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2
 library(dplyr)
 
 humdev <- rename(hd, HDIRank = HDI.Rank, Country = Country, HDI = Human.Development.Index..HDI.
-                 , LifeExpectancy = Life.Expectancy.at.Birth, ExpectedEducation= Expected.Years.of.Education, 
+                 , Life.Exp = Life.Expectancy.at.Birth, Edu.Exp= Expected.Years.of.Education, 
                  Education = Mean.Years.of.Education, GNI = Gross.National.Income..GNI..per.Capita, 
                  GNI_HDI =GNI.per.Capita.Rank.Minus.HDI.Rank )
 dim(humdev)
@@ -18,12 +18,12 @@ str(humdev)
 summary(humdev)
 
 gend <- rename(gii, GIIRank = GII.Rank, GII= Gender.Inequality.Index..GII., 
-                 Mort = Maternal.Mortality.Ratio, 
-                 BR = Adolescent.Birth.Rate,
-                 Rep = Percent.Representation.in.Parliament,
-                 EduF = Population.with.Secondary.Education..Female.,
+                 Mat.Mor = Maternal.Mortality.Ratio, 
+               Ado.Birth = Adolescent.Birth.Rate,
+               Parli.F = Percent.Representation.in.Parliament,
+               Edu2.FM = Population.with.Secondary.Education..Female.,
                  EduM = Population.with.Secondary.Education..Male.,
-                 LabF = Labour.Force.Participation.Rate..Female.,
+               Labo.FM = Labour.Force.Participation.Rate..Female.,
                  LabM = Labour.Force.Participation.Rate..Male.)
 
 dim(gend)
@@ -32,7 +32,7 @@ summary(gend)
 
 #Create the new variables
 
-gend <- mutate(gend, LabRatio = LabF / LabM, EduRatio = EduF / EduM)
+gend <- mutate(gend, LabRatio = Labo.FM / LabM, EduRatio = Edu2.FM / EduM)
 
 #Combine the data and check if everything is okay
 human <- inner_join(humdev,gend,by= "Country")
@@ -40,4 +40,13 @@ dim(human)
 str(human)
 
 #write the data to data folder
-write.table(alc, file='data/human.txt')
+write.table(alc, file='data/human.txt') 
+
+#replace "," with "." in GNI and transform it to numeric
+human$GNI <- gsub(",", ".", human$GNI)
+human$GNI <- as.numeric(human$GNI)
+
+#select only certain variables
+selected_variables <- c( "Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+human <- select(human, one_of(selected_variables))
+str(human)
